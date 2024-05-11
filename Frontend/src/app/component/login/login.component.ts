979402046fc30f61dc50ed7472b29fb00fc769fd
KissSergiuF <kisssergiuf@gmail.com>
2024-05-11 19:04:20 +0300

    Login

--- Frontend/src/app/component/login/login.component.ts
+++ Frontend/src/app/component/login/login.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { LoginService } from '../../service/login.service';
+import { User } from '../../model/user.model';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent { 
-  constructor(private http: HttpClient) {
-    
+  email: string;
+  password: string;
+  loginActual: User | undefined;
+  constructor(private http: HttpClient, private loginService: LoginService) {
+    this.email = '';
+    this.password = '';
   }
-    
-  
+  onLogin(){
+    this.loginService.getUser(this.email, this.password).subscribe(data => {
+      this.loginActual = data;
+      console.log(this.loginActual)
+    });
+  }
 }
-
