995aa26d22ad24f810f6cbd92ce41a32bd1634ac
Anubis1960 <alexander.rk19@gmail.com>
2024-05-13 20:22:56 +0300

    pop up

--- Frontend/src/app/component/course/course.component.ts
+++ Frontend/src/app/component/course/course.component.ts
@@ -5,6 +5,8 @@ import { Course } from '../../model/course.model';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
+import { MatDialog } from '@angular/material/dialog';
+import { PopUpComponent } from '../pop-up/pop-up.component';
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
@@ -16,23 +18,12 @@ export class CourseComponent implements OnInit {
   dataSource: any;
   displyedColumns: string[]=['id', 'name', 'description', 'category', 'facultySection', 'maximumStudentsAllowed','numberOfStudents','teacherName','year','action'];
 
-  constructor(private http: HttpClient, private courseService: CourseService) { }
+  constructor(private http: HttpClient, private courseService: CourseService, private dialog: MatDialog) { }
 
   @ViewChild(MatPaginator) paginator !: MatPaginator;
   @ViewChild(MatSort) sort !: MatSort;
   ngOnInit(): void {
-    this.courseService.getCoursesList().subscribe({
-      next: (data: Course[]) => {
-        this.courses = data;
-        this.dataSource = new MatTableDataSource<Course>(this.courses);
-        this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
-        console.log(this.courses);
-      },
-      error: (error) => {
-        console.log(error);
-      }
-    });
+    this.refresh();
   }
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator
@@ -43,18 +34,45 @@ export class CourseComponent implements OnInit {
     this.courseService.deleteCourse(id).subscribe({
       next: (data: Course) => {
         console.log(data);
-        this.courseService.getCoursesList().subscribe({
-          next: (data: Course[]) => {
-            this.courses = data;
-            this.dataSource = new MatTableDataSource<Course>(this.courses);
-            this.dataSource.paginator = this.paginator;
-            this.dataSource.sort = this.sort;
-            console.log(this.courses);
-          },
-          error: (error) => {
-            console.log(error);
-          }
-        });
+        this.refresh();
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  }
+
+  openDialog(id:number,name : string, description: string, category: string, facultySection: string, maximumStudentsAllowed: number, teacherName: string, year: number, title: string) {
+    this.dialog.open(PopUpComponent, {
+      width: '400px',
+      height: '600px',
+      data: {
+        title: title,
+        id: id,
+        name: name,
+        description: description,
+        category: category,
+        facultySection: facultySection,
+        maximumStudentsAllowed: maximumStudentsAllowed,
+        teacherName: teacherName,
+        year: year
+      }
+    });
+    this.dialog.afterAllClosed.subscribe({
+      next: (data) => {
+        this.refresh();
+      }
+    });
+  }
+
+  refresh() {
+    this.courseService.getCoursesList().subscribe({
+      next: (data: Course[]) => {
+        this.courses = data;
+        this.dataSource = new MatTableDataSource<Course>(this.courses);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        console.log(this.courses);
       },
       error: (error) => {
         console.log(error);
